4f96871cf39430230913e338b518db21e312e9c7
Dody Goutama <glodyk@gmail.com>
2026-02-25 00:39:09 +0700

    Remove research and exploratory files from warehouse repository

--- cari diag_pros.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  nmppklayan,
-  COUNT(DISTINCT nosjp) kasus_transfusi
-FROM `bpjs-kediri.enriched.inacbgs_enriched`
-WHERE '3995' IN UNNEST(prosedur_kode)
-GROUP BY nmppklayan
-ORDER BY kasus_transfusi DESC;
